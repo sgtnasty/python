f326dff4c1ae8349eee9c3842a1200fbdb22cbf7
Ronaldo Nascimento <sgtnasty@gmail.com>
2017-06-29 16:19:23 -0400

    fixed indents

--- nine/nine.py
+++ nine/nine.py
@@ -4,7 +4,6 @@
 def digital_root(n):
     """
     keep summing the digits until you get a single-digit number
-    
     https://stackoverflow.com/a/39734394/142798
     """
     x = sum(int(digit) for digit in str(n))
@@ -17,13 +16,13 @@ def digital_root(n):
 def main(maxint):
     print('nine')
     for i in range(maxint):
-    	v = digital_root(i)
-    	if (v == 3):
-    		print('**  %d:  %d' % (i, v))
-    	elif (v == 6):
-    		print('*   %d:   %d' % (i, v))
-    	elif (v == 9):
-    		print('*** %d: %d' % (i, v))
+        v = digital_root(i)
+        if (v == 3):
+            print('**  %d:  %d' % (i, v))
+        elif (v == 6):
+            print('*   %d:   %d' % (i, v))
+        elif (v == 9):
+            print('*** %d: %d' % (i, v))
 
 
 if __name__ == '__main__':
