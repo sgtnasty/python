248feb942e53be6ac9da2ae92e975f1747183c79
Ronaldo Nascimento <sgtnasty@gmail.com>
2017-06-29 16:52:48 -0400

    working on randomizer
    added info file

---
+++ life/README.md
@@ -0,0 +1,10 @@
+# life
+
+`kwatz!`
+
+Experimentations in AI
+
+I am no AI expert, just really likes Ummon in the Hyperion Cantos
+
+http://tycho.usno.navy.mil/cgi-bin/timer.pl
+
--- life/life.py
+++ life/life.py
@@ -1,12 +1,26 @@
 #!/usr/bin/env python3
 
 
-import os, sys, random
+import os
+import sys
+import random
 
 
 def main():
     print('life begins')
+    r = random.SystemRandom()
+    for i in range(r.randrange(1, 256)):
+        v = r.randint(1, 256)
+        print('%d: %d' % (i, v))
 
 
 if __name__ == "__main__":
-    main()
+    try:
+        print('%s running on %s' % (sys.argv[0], os.name))
+        main()
+    except OSError as err:
+        print("OSError: %s" % err)
+    except IndexError as err:
+        print("IndexError: %s" % err)
+    except:
+        print("Unexpected error: %s" % sys.exc_info()[0])
