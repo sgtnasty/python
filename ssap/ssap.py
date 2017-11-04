996769d5a48e24dc5df673aa105020f3542019cd
Ronaldo Nascimento <sgtnasty@gmail.com>
2017-11-04 05:30:18 -0400

    new algorithms included

--- ssap/ssap.py
+++ ssap/ssap.py
@@ -4,8 +4,31 @@ import os
 import uuid
 
 
-if __name__ == '__main__':
-    d = os.urandom(32)
+def generate_uuid():
     s = uuid.uuid1()
     print(s)
     print(repr(s.fields))
+
+
+def generate_temp_password(length):
+    if not isinstance(length, int) or length < 8:
+        raise ValueError("temp password must have positive length")
+    chars = "ABCDEFGHJKLMNPQRSTUVWXYZ23456789abcdefghijklmnopqrstuvwxyz"
+    return "".join(chars[ord(c) % len(chars)] for c in os.urandom(length))
+
+
+def generate_urandom(length):
+    if not isinstance(length, int) or length < 8:
+        raise ValueError("temp password must have positive length")
+    d = os.urandom(length)
+    v = map(ord, os.urandom(length))
+    return v
+
+
+if __name__ == '__main__':
+    v = generate_urandom(32)
+    print(v)
+    v = generate_temp_password(16)
+    print(v)
+    v = generate_temp_password(32)
+    print(v)
